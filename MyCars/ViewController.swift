56a4503b2ee2797001176f77671753f58df00022
Vladimir Kravets <natccorp@gmail.com>
2022-10-24 23:45:59 +0300

    first start and only one save getDataFromFile() in CoreData

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -12,8 +12,17 @@ import CoreData
 
 class ViewController: UIViewController {
     
+    let defaults = UserDefaults.standard
+    
+    lazy var dateFormator: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .short
+        df.timeStyle = .none
+        return df
+    }()
+    
     var context: NSManagedObjectContext!
-
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     
     @IBOutlet weak var markLabel: UILabel!
@@ -30,11 +39,42 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myChoiceImageView: UIImageView!
     
+    private func insertDataFrom(selectedCar car: Car) {
+        
+        carImageView.image = UIImage(data: car.imageData!)
+        markLabel.text = car.mark
+        modelLabel.text = car.model
+        myChoiceImageView.isHidden = !(car.myChoice)
+        ratingLabel.text = "Rating: \(car.rating) / 10"
+        numberOfTripsLabel.text = "Number of trips: \(car.timesDriven)"
+        lastTimeStartedLabel.text = "Last time started: \(dateFormator.string(from: car.lastStarted!))"
+        segmentedControl.tintColor = car.tintColor as? UIColor
+        
+    }
+    
     
     //MARK: -func add data.plist in app
     private func getDataFromFile() {
+
+        
+        //MARK: -check all records type Car in CoreData
+//        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+//        fetchRequest.predicate = NSPredicate(format: "mark != nil")
+//
+//        var records = 0
+//
+//        do {
+//            records = try context.count(for: fetchRequest)
+//            print("Is Data there already ?")
+//        } catch let error as NSError {
+//            print(error.localizedDescription)
+//        }
+//        guard records == 0 else {return print("Is empty")}
+//        print("Data there")
+        
+        
         guard let pathToFile = Bundle.main.path(forResource: "data", ofType: "plist"),
-                let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
+              let dataArray = NSArray(contentsOfFile: pathToFile) else { return }
         
         //MARK: -extract dictionary from data.plist
         for dictionary in dataArray {
@@ -52,16 +92,23 @@ class ViewController: UIViewController {
             
             //MARK: -working with image
             guard let imageName = carDictionary["imageName"] as? String,
-                let image = UIImage(named: imageName) else {return}
+                  let image = UIImage(named: imageName) else {return}
             let imageData = image.pngData()
-            car.imageData = imageData as? Data
+            car.imageData = imageData
             
             //MARK: - tintColor
             if let colorDictionary = carDictionary["tintColor"] as? [String : Float] {
                 car.tintColor = getColor(colorDictionary: colorDictionary)
             }
+    }
+        do {
+            try context.save()
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
         }
     }
+    
     //MARK: get color
     private func getColor(colorDictionary: [String : Float]) -> UIColor {
         guard let red = colorDictionary["red"],
@@ -70,19 +117,38 @@ class ViewController: UIViewController {
         return UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1.0)
     }
     
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        //MARK: -How to check if the IOS app is running for the first time using SWIFT
-        if (UserDefaults.standard.bool(forKey: "HasLaunchedOne")) {
-        print("HasLaunchedOne")
+        //MARK: -first start and only one save getDataFromFile() in CoreData
+        if defaults.bool(forKey: "First Lunch") == true {
+            print("Second")
+            chooseSegment()
+            defaults.set(true, forKey: "First Lunch")
         } else {
-            UserDefaults.standard.set(true, forKey: "HasLaunchedOne")
-            UserDefaults.standard.synchronize()
+            print("First session")
             getDataFromFile()
-            print("First Launch")
+            chooseSegment()
+            defaults.set(true, forKey: "First Lunch")
         }
+        
+
+       //MARK: -make choose that show on segmentedControl at first VC
+        func chooseSegment() {
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        let mark = segmentedControl.titleForSegment(at: 0)
+        fetchRequest.predicate = NSPredicate(format: "mark == %@", mark!)
+        do {
+            let results = try context.fetch(fetchRequest)
+            let selectedCar = results.first
+            insertDataFrom(selectedCar: selectedCar!)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
     }
     @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
     }
@@ -95,5 +161,23 @@ class ViewController: UIViewController {
     }
     
     
+    @IBAction func deleteButton(_ sender: Any) {
+        
+        let fetchRequest: NSFetchRequest<Car> = Car.fetchRequest()
+        
+        if let result = try? context.fetch(fetchRequest) {
+            for object in result {
+                context.delete(object)
+                print("delete")
+            }
+            
+        }
+        do {
+            try context.save()
+            
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+   }
 }
 
